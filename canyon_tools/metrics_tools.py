bcd0d7703fca752d5a12114c4952bf2435c276d6
Karina Ramos Musalem <kramosmu@eoas.ubc.ca>
2016-01-27 16:05:49 -0800

    Added functions get_TRAC and slice_TRAC to MetricsPythonTools.py

--- canyon_tools/metrics_tools.py
+++ canyon_tools/metrics_tools.py
@@ -27,53 +27,52 @@ import ReadOutTools_MITgcm as rout
 
 #---------------------------------------------------------------------------------------------------------------------------
 def getDatasets(expPath, runName):
-    '''Specify the experiment and run from which to analyse state and ptracers output.
-    expName : (string) Path to experiment folder. E.g. '/ocean/kramosmu/MITgcm/TracerExperiments/BARKLEY', etc.
-    runName : (string) Folder name of the run. E.g. 'run01', 'run10', etc
-    '''
-    Grid =   "%s/%s/gridGlob.nc" %(expPath,runName)
-    GridOut = Dataset(Grid)
+  '''Specify the experiment and run from which to analyse state and ptracers output.
+   expName : (string) Path to experiment folder. E.g. '/ocean/kramosmu/MITgcm/TracerExperiments/BARKLEY', etc.
+   runName : (string) Folder name of the run. E.g. 'run01', 'run10', etc
+  '''
+  Grid = "%s/%s/gridGlob.nc" %(expPath,runName)
+  GridOut = Dataset(Grid)
 
-    State =  "%s/%s/stateGlob.nc" %(expPath,runName)
-    StateOut = Dataset(State)
+  State =  "%s/%s/stateGlob.nc" %(expPath,runName)
+  StateOut = Dataset(State)
 
-    Ptracers =  "%s/%s/ptracersGlob.nc" %(expPath,runName)
-    PtracersOut = Dataset(Ptracers)
+  Ptracers =  "%s/%s/ptracersGlob.nc" %(expPath,runName)
+  PtracersOut = Dataset(Ptracers)
     
-    return (Grid, GridOut, State,StateOut,Ptracers, PtracersOut)
+  return (Grid, GridOut, State,StateOut,Ptracers, PtracersOut)
 
 
 #---------------------------------------------------------------------------------------------------------------------------
 
 
 def getProfile(Tr,yi,xi,nz0=0,nzf=90):
-    '''Slice tracer profile at x,y = xi,yi form depth index k=nz0 to k=nzf. Default values are nz0=0 (surface)
-    and nzf = 89, bottom. Tr is a time slice (3D) of the tracer field'''
-    IniProf = Tr[nz0:nzf,yi,xi]
-    return IniProf
+  '''Slice tracer profile at x,y = xi,yi form depth index k=nz0 to k=nzf. Default values are nz0=0 (surface)
+  and nzf = 89, bottom. Tr is a time slice (3D) of the tracer field'''
+  IniProf = Tr[nz0:nzf,yi,xi]
+  return IniProf
 
 
 #---------------------------------------------------------------------------------------------------------------------------
 
 
 def maskExpand(mask,Tr):
+  '''Expand the dimensions of mask to fit those of Tr. mask should have one dimension less than Tr (time axis). 
+  It adds a dimension before the first one.'''
     
-    '''Expand the dimensions of mask to fit those of Tr. mask should have one dimension less than Tr (time axis). 
-    It adds a dimension before the first one.'''
+  mask_expand = np.expand_dims(mask,0)
     
-    mask_expand = np.expand_dims(mask,0)
+  mask_expand = mask_expand + np.zeros(Tr.shape)
     
-    mask_expand = mask_expand + np.zeros(Tr.shape)
-    
-    return mask_expand
+  return mask_expand
 
     
 #---------------------------------------------------------------------------------------------------------------------------
 
 
 def howMuchWaterX(Tr,MaskC,nzlim,rA,hFacC,drF,yin,zfin,xi,yi):
-    '''
-    INPUT----------------------------------------------------------------------------------------------------------------
+  '''
+  INPUT----------------------------------------------------------------------------------------------------------------
     Tr    : Array with concentration values for a tracer. Until this function is more general, this should be size 19x90x360x360
     MaskC : Land mask for tracer
     nzlim : The nz index under which to look for water properties
@@ -90,44 +89,44 @@ def howMuchWaterX(Tr,MaskC,nzlim,rA,hFacC,drF,yin,zfin,xi,yi):
     Total_Tracer =  Array with the mass of tracer (m^3*[C]*l/m^3) at each x-position over the shelf [:,:30,227:,:] at 
                     every time output. Total mass of tracer at xx on the shelf.
                                                 
-    -----------------------------------------------------------------------------------------------------------------------
-    '''
-    maskExp = maskExpand(MaskC,Tr)
+  -----------------------------------------------------------------------------------------------------------------------
+  '''
+  maskExp = maskExpand(MaskC,Tr)
 
-    TrMask=np.ma.array(Tr,mask=maskExp)   
+  TrMask=np.ma.array(Tr,mask=maskExp)   
     
-    trlim = TrMask[0,nzlim,yi,xi]
+  trlim = TrMask[0,nzlim,yi,xi]
     
-    print('tracer limit concentration is: ',trlim)
+  print('tracer limit concentration is: ',trlim)
     
-    WaterX = 0
+  WaterX = 0
     
-    # mask cells with tracer concentration < trlim on shelf
-    HighConc_Masked = np.ma.masked_less(TrMask[:,:zfin,yin:,:], trlim) 
-    HighConc_Mask = HighConc_Masked.mask
+  # mask cells with tracer concentration < trlim on shelf
+  HighConc_Masked = np.ma.masked_less(TrMask[:,:zfin,yin:,:], trlim) 
+  HighConc_Mask = HighConc_Masked.mask
     
-    #Get volume of water of cells with relatively high concentration
-    rA_exp = np.expand_dims(rA[yin:,:],0)
-    drF_exp = np.expand_dims(np.expand_dims(drF[:zfin],1),1)
-    rA_exp = rA_exp + np.zeros(hFacC[:zfin,yin:,:].shape)
-    drF_exp = drF_exp + np.zeros(hFacC[:zfin,yin:,:].shape)
+  #Get volume of water of cells with relatively high concentration
+  rA_exp = np.expand_dims(rA[yin:,:],0)
+  drF_exp = np.expand_dims(np.expand_dims(drF[:zfin],1),1)
+  rA_exp = rA_exp + np.zeros(hFacC[:zfin,yin:,:].shape)
+  drF_exp = drF_exp + np.zeros(hFacC[:zfin,yin:,:].shape)
     
-    ShelfVolume = hFacC[:zfin,yin:,:]*drF_exp*rA_exp
-    ShelfVolume_exp = np.expand_dims(ShelfVolume,0)
-    ShelfVolume_exp = ShelfVolume_exp + np.zeros(HighConc_Mask.shape)
+  ShelfVolume = hFacC[:zfin,yin:,:]*drF_exp*rA_exp
+  ShelfVolume_exp = np.expand_dims(ShelfVolume,0)
+  ShelfVolume_exp = ShelfVolume_exp + np.zeros(HighConc_Mask.shape)
     
-    HighConc_CellVol = np.ma.masked_array(ShelfVolume_exp,mask = HighConc_Mask) 
-    VolWaterHighConc = np.ma.sum(np.ma.sum(np.ma.sum(HighConc_CellVol,axis = 1),axis=1),axis=1)
+  HighConc_CellVol = np.ma.masked_array(ShelfVolume_exp,mask = HighConc_Mask) 
+  VolWaterHighConc = np.ma.sum(np.ma.sum(np.ma.sum(HighConc_CellVol,axis = 1),axis=1),axis=1)
     
-    #Get total mass of tracer on shelf
-    Total_Tracer = np.ma.sum(np.ma.sum(np.ma.sum(ShelfVolume_exp*TrMask[:,:zfin,yin:,:]*1000.0,axis = 1),axis=1),axis=1) 
-    # 1 m^3 = 1000 l
+   #Get total mass of tracer on shelf
+  Total_Tracer = np.ma.sum(np.ma.sum(np.ma.sum(ShelfVolume_exp*TrMask[:,:zfin,yin:,:]*1000.0,axis = 1),axis=1),axis=1) 
+   # 1 m^3 = 1000 l
     
-    return (VolWaterHighConc, Total_Tracer)
+  return (VolWaterHighConc, Total_Tracer)
 
 #---------------------------------------------------------------------------------------------------------------------------
 def howMuchWaterCV(Tr,MaskC,nzlim,rA,hFacC,drF,yin,zfin,xi,yi,xo,xf):
-    '''
+  '''
     INPUT----------------------------------------------------------------------------------------------------------------
     Tr    : Array with concentration values for a tracer. Until this function is more general, this should be size 19x90x360x360
     MaskC : Land mask for tracer
@@ -147,42 +146,75 @@ def howMuchWaterCV(Tr,MaskC,nzlim,rA,hFacC,drF,yin,zfin,xi,yi,xo,xf):
                     every time output. Total mass of tracer at xx on the shelf.
                                                 
     -----------------------------------------------------------------------------------------------------------------------
-    '''
-    maskExp = maskExpand(MaskC,Tr)
+  '''
+  maskExp = maskExpand(MaskC,Tr)
 
-    TrMask=np.ma.array(Tr,mask=maskExp)   
+  TrMask=np.ma.array(Tr,mask=maskExp)   
     
-    trlim = TrMask[0,nzlim,yi,xi]
+  trlim = TrMask[0,nzlim,yi,xi]
     
-    print('tracer limit concentration is: ',trlim)
+  print('tracer limit concentration is: ',trlim)
     
-    WaterX = 0
+  WaterX = 0
     
-    # mask cells with tracer concentration < trlim on control volume
-    HighConc_Masked = np.ma.masked_less(TrMask[:,:zfin,yin:,xo:xf], trlim) 
-    HighConc_Mask = HighConc_Masked.mask
+  # mask cells with tracer concentration < trlim on control volume
+  HighConc_Masked = np.ma.masked_less(TrMask[:,:zfin,yin:,xo:xf], trlim) 
+  HighConc_Mask = HighConc_Masked.mask
     
-    #Get volume of water of cells with relatively high concentration
-    rA_exp = np.expand_dims(rA[yin:,xo:xf],0)
-    drF_exp = np.expand_dims(np.expand_dims(drF[:zfin],1),1)
-    rA_exp = rA_exp + np.zeros(hFacC[:zfin,yin:,xo:xf].shape)
-    drF_exp = drF_exp + np.zeros(hFacC[:zfin,yin:,xo:xf].shape)
+  #Get volume of water of cells with relatively high concentration
+  rA_exp = np.expand_dims(rA[yin:,xo:xf],0)
+  drF_exp = np.expand_dims(np.expand_dims(drF[:zfin],1),1)
+  rA_exp = rA_exp + np.zeros(hFacC[:zfin,yin:,xo:xf].shape)
+  drF_exp = drF_exp + np.zeros(hFacC[:zfin,yin:,xo:xf].shape)
     
-    ShelfVolume = hFacC[:zfin,yin:,xo:xf]*drF_exp*rA_exp
-    ShelfVolume_exp = np.expand_dims(ShelfVolume,0)
-    ShelfVolume_exp = ShelfVolume_exp + np.zeros(HighConc_Mask.shape)
+  ShelfVolume = hFacC[:zfin,yin:,xo:xf]*drF_exp*rA_exp
+  ShelfVolume_exp = np.expand_dims(ShelfVolume,0)
+  ShelfVolume_exp = ShelfVolume_exp + np.zeros(HighConc_Mask.shape)
     
-    HighConc_CellVol = np.ma.masked_array(ShelfVolume_exp,mask = HighConc_Mask) 
-    VolWaterHighConc = np.ma.sum(np.ma.sum(np.ma.sum(HighConc_CellVol,axis = 1),axis=1),axis=1)
+  HighConc_CellVol = np.ma.masked_array(ShelfVolume_exp,mask = HighConc_Mask) 
+  VolWaterHighConc = np.ma.sum(np.ma.sum(np.ma.sum(HighConc_CellVol,axis = 1),axis=1),axis=1)
     
     #Get total mass of tracer on shelf
-    Total_Tracer = np.ma.sum(np.ma.sum(np.ma.sum(ShelfVolume_exp*TrMask[:,:zfin,yin:,xo:xf]*1000.0,axis = 1),axis=1),axis=1) 
+  Total_Tracer = np.ma.sum(np.ma.sum(np.ma.sum(ShelfVolume_exp*TrMask[:,:zfin,yin:,xo:xf]*1000.0,axis = 1),axis=1),axis=1) 
     # 1 m^3 = 1000 l
     
-    return (VolWaterHighConc, Total_Tracer)
+  return (VolWaterHighConc, Total_Tracer)
 
 
+#----------------------------------------------------------------------------------------------------------------------------
 
+def get_TRAC(fluxFile, keyW, keyV, keyU):
+  ''' all input are strings'''
+  WTRAC = rout.getField(fluxFile,keyW)
+  UT = rout.getField(fluxFile,keyU)
+  VT = rout.getField(fluxFile,keyV)
+  UTRAC,VTRAC = rout.unstagger(UT,VT)
+    
+  return (WTRAC, VTRAC, UTRAC)
 
+#----------------------------------------------------------------------------------------------------------------------------
 
+def slice_TRAC(field,i1,i2,j1,j2,k1,k2,t1,t2):
+  '''i1 initial x index, i2 final x index and so on. Integers. Returns sliced field'''
+  if i1 == i2:
+    sl = field[t1:t2,k1:k2,j1:j2,i1]
+  elif j1==j2:
+    sl = field[t1:t2,k1:k2,j1,i1:i2]
+  elif k1 == k2:
+    sl = field[t1:t2,k1,j1:j2,i1:i2]
+  elif t1 == t2:
+    sl = field[t1,k1:k2,j1:j2,i1:i2]
+  else:
+    sl = field[t1:t2,k1:k2,j1:j2,i1:i2]
+  
+  return sl
+  
+#---------------------------------------------------------------------------------------------------------------------------------  
+  
+  
+  
+  
+  
+  
+  
 
