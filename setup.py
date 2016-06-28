88872da29bef2bd2e2a1670262bcb44f2daf6c4b
Karina Ramos Musalem <kramosmu@eoas.ubc.ca>
2016-06-28 15:43:09 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='CanyonTools',
+    version='1.0',
+    description='Useful functions to analyze canyon experiments',
+    author='Karina Ramos Musalem',
+    author_email='kramosmu@eos.ubc.ca',
+    install_requires=[],
+    packages=['canyon_tools'],
+)
+
+ 
